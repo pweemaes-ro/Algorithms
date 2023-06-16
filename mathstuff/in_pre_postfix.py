1dec9c4cb6412f147e128300ec9666fba1eb26b9
pweemaes-ro <zappamotel@quicknet.nl>
2023-06-16 15:18:28 +0200

    Seems to work...

--- mathstuff/in_pre_postfix.py
+++ mathstuff/in_pre_postfix.py
@@ -5,7 +5,7 @@ from typing import TypeVar, TypeAlias, Optional
 
 T = TypeVar('T')
 
-__precedence: dict[str, int] = {'^': 6, '*': 5, '/': 5, '+': 3, '-': 3}
+__precedence: dict[str, int] = {'^': 4, '*': 3, '/': 3, '+': 2, '-': 2}
 __right_associative_operators = set("^")
 __left_associative_operators = set("+-*/")
 __operators = __right_associative_operators.union(__left_associative_operators)
@@ -15,6 +15,39 @@ Operator: TypeAlias = str
 Operand: TypeAlias = str
 OperatorStack: TypeAlias = list[Operator]
 OperandStack: TypeAlias = list[Operand]
+
+
+def _is_operand(symbol: str) -> bool:
+	return len(symbol) == 1 and symbol.upper() in __operands
+
+
+def _is_operator(symbol: str) -> bool:
+	return symbol in __operators
+
+
+def _pop_operator(operator_stack: OperatorStack, operator: Operator) \
+	-> Optional[Operator]:
+	# i. Pop operator on the stack that
+	#       a. is above the most recently scanned left parenthesis, and
+	#       b. has precedence higher than or is a right-associative
+	#          operator of equal precedence to that of the new operator
+	#          symbol.
+
+	if len(operator_stack) == 0:
+		return None
+
+	stack_operator = operator_stack[-1]
+	if stack_operator not in __operators:
+		return None
+
+	if __precedence[stack_operator] > __precedence[operator]:
+		return operator_stack.pop()
+
+	if __precedence[stack_operator] == __precedence[operator]:
+		if operator in __left_associative_operators:
+			return operator_stack.pop()
+
+	return None
 
 
 def postfix_to_infix(postfix: str) -> str:
@@ -32,41 +65,6 @@ def postfix_to_infix(postfix: str) -> str:
 			operand_stack.append(c)
 	
 	return operand_stack.pop()[1:-1]
-
-
-def _is_operand(symbol: str) -> bool:
-	return len(symbol) == 1 and symbol.upper() in __operands
-
-
-def _is_operator(symbol: str) -> bool:
-	return symbol in __operators
-
-
-def _is_right_associative(operator: Operator) -> bool:
-	return len(operator) == 1 and operator in __right_associative_operators
-
-
-def _pop_operator(operator_stack: OperatorStack, operator: Operator) \
-	-> Optional[Operator]:
-	# i. Pop operator on the stack that
-	#       a. is above the most recently scanned left parenthesis, and
-	#       b. has precedence higher than or is a right-associative
-	#          operator of equal precedence to that of the new operator
-	#          symbol.
-	
-	if len(operator_stack):
-		next_operator = operator_stack[-1]
-		if next_operator == "(":
-			return None
-		
-		if __precedence[next_operator] >= __precedence[operator]:
-			return operator_stack.pop()
-		
-		if _is_right_associative(next_operator) and \
-				__precedence[next_operator] == __precedence[operator]:
-			return operator_stack.pop()
-	
-	return None
 
 
 def postfix_to_prefix(postfix: str) -> str:
@@ -156,7 +154,7 @@ def infix_to_postfix(infix: str) -> str:
 def infix_to_prefix(infix: str) -> str:
 	"""Return the prefix representation of the infix expression (supports
 	operators: ^, *, /, + and -."""
-	
+
 	return postfix_to_prefix(infix_to_postfix(infix))
 
 
--- tests/mathstuff/test_in_pre_post.py
+++ tests/mathstuff/test_in_pre_post.py
@@ -2,7 +2,6 @@
 from in_pre_postfix import infix_to_postfix, postfix_to_infix, \
 	infix_to_prefix, prefix_to_postfix, prefix_to_infix, postfix_to_prefix
 
-
 test_in_post_pre = (
 	("A+B-C", "AB+C-", "-+ABC"),
 	("A+B*C", "ABC*+", "+A*BC"),
@@ -10,6 +9,17 @@ test_in_post_pre = (
 	("((A+B)*(C-D)+E)/(F+G)", "AB+CD-*E+FG+/", "/+*+AB-CDE+FG"),
 	("A+B*C/D-E", "ABC*D/+E-", "-+A/*BCDE"),
 	("(A+B*(C-D))/E", "ABCD-*+E/", "/+A*B-CDE"),
+	("A+B", "AB+", "+AB"),
+	("A+B*C", "ABC*+", "+A*BC"),
+	("(A+B)*C", "AB+C*", "*+ABC"),
+	("A+B-C", "AB+C-", "-+ABC"),
+	("A*B+C", "AB*C+", "+*ABC"),
+	("A*(B+C)", "ABC+*", "*A+BC"),
+	("A^B^C", "ABC^^", "^A^BC"),
+	("A^B*C", "AB^C*", "*^ABC"),
+	("A^(B*C)", "ABC*^", "^A*BC"),
+	("(A^B)*C", "AB^C*", "*^ABC"),
+	("A+(B*C^D-E)^(F+G*H)-I", 'ABCD^*E-FGH*+^+I-', "-+A^-*B^CDE+F*GHI"),
 )
 
 
@@ -20,12 +30,18 @@ def test_infix_to_postfix() -> None:
 
 def test_infix_to_prefix() -> None:
 	for infix, _, prefix in test_in_post_pre:
-		assert infix_to_prefix(infix) == prefix
+		assert infix_to_prefix(infix) == prefix, \
+			f"{infix=}, " \
+			f"expected (my tests): {prefix}, " \
+			f"got: {infix_to_prefix(infix)}"
 
 
 def test_any_to_infix() -> None:
-	for _, postfix, prefix in test_in_post_pre:
-		assert postfix_to_infix(postfix) == prefix_to_infix(prefix)
+	for infix, postfix, prefix in test_in_post_pre:
+		infix_from_postfix = postfix_to_infix(postfix)
+		infix_from_prefix = prefix_to_infix(prefix)
+		assert infix_from_postfix == infix_from_prefix, \
+			f"{infix = }, {infix_from_postfix = }, {infix_from_prefix = }"
 
 
 def test_prefix_to_postfix() -> None:
