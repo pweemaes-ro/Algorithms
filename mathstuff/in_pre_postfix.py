f1db4bbff3d89a87cca9e1a56b5b44bfb8867420
pweemaes-ro <zappamotel@quicknet.nl>
2023-06-17 09:42:04 +0200

    Now supports multichar operators and operands, but expects space-separated input (infix, prefix, postfix).

--- mathstuff/in_pre_postfix.py
+++ mathstuff/in_pre_postfix.py
@@ -3,64 +3,63 @@ from collections.abc import Callable
 from string import ascii_uppercase
 from typing import TypeAlias, Optional
 
-
-__precedence: dict[str, int] = {'^': 4, '*': 3, '/': 3, '+': 2, '-': 2}
-__right_associative_operators = set("^")
-__left_associative_operators = set("+-*/")
-__operators = __right_associative_operators.union(__left_associative_operators)
-__operands = set(ascii_uppercase)
-
 Operator: TypeAlias = str
-Operators: TypeAlias = set[Operator]
+Precedence: TypeAlias = int
+Operators: TypeAlias = dict[Operator, Precedence]
 Operand: TypeAlias = str
 OperatorStack: TypeAlias = list[Operator]
 OperandStack: TypeAlias = list[Operand]
 PopFunction: TypeAlias = Callable[[OperatorStack, Operator], Optional[Operator]]
 
+__exponential_operator = "^"   # Choose "**" (Python) or "^" (most other...)
+__right_associative_operators = {__exponential_operator: 4}
+__left_associative_operators = {"*": 3, "/": 3, "+": 2, "-": 2}
+__operators = __right_associative_operators | __left_associative_operators
+__operands = set(ascii_uppercase)
+
+
+def _precedence(operator: Operator) -> int:
+	return __operators[operator]
+
 
 def _is_operand(symbol: str) -> bool:
-	return len(symbol) == 1 and symbol.upper() in __operands
+	return symbol.upper() in __operands
 
 
 def _is_operator(symbol: str) -> bool:
 	return symbol in __operators
 
 
-def _pop_any(operator_stack: OperatorStack, operator: Operator,
-             associative_operators: Operators) -> Optional[Operator]:
+def _pop_any(operator_stack: OperatorStack, new_operator: Operator,
+             associative_ops: Operators) -> Optional[Operator]:
 	"""Pop and return from *operator_stack* if top of stack is an operator that
 	1. has higher precedence than *operator*, or
 	2. has equal precedence than *operator* and *operator* is in the set of
 	   associate operators."""
+	
+	if len(operator_stack) \
+		and _is_operator(stack_operator := operator_stack[-1]):
 
-	if len(operator_stack) == 0:
-		return None
-
-	stack_operator = operator_stack[-1]
-
-	if not _is_operator(stack_operator):
-		return None
-
-	if __precedence[stack_operator] > __precedence[operator]:
-		return operator_stack.pop()
-
-	if __precedence[stack_operator] == __precedence[operator]:
-		if operator in associative_operators:
+		if _precedence(stack_operator) > _precedence(new_operator):
+			return operator_stack.pop()
+		
+		if _precedence(stack_operator) == _precedence(new_operator) \
+				and new_operator in associative_ops:
 			return operator_stack.pop()
 
 	return None
 
 
-def _pop_postfix(operator_stack: OperatorStack, operator: Operator) \
-	-> Optional[Operator]:
+def _pop_postfix(operator_stack: OperatorStack, new_operator: Operator) \
+		-> Optional[Operator]:
+	
+	return _pop_any(operator_stack, new_operator, __left_associative_operators)
 
-	return _pop_any(operator_stack, operator, __left_associative_operators)
 
-
-def _pop_prefix(operator_stack: OperatorStack, operator: Operator) \
-	-> Optional[Operator]:
-
-	return _pop_any(operator_stack, operator, __right_associative_operators)
+def _pop_prefix(operator_stack: OperatorStack, new_operator: Operator) \
+		-> Optional[Operator]:
+	
+	return _pop_any(operator_stack, new_operator, __right_associative_operators)
 
 
 def _swap_chars(string: str, char_1: str, char_2: str) -> str:
@@ -79,19 +78,19 @@ def _swap_chars(string: str, char_1: str, char_2: str) -> str:
 
 
 def _postfix_to_infix(postfix: str) -> str:
-	
 	operand_stack: OperandStack = []
 	
-	for symbol in postfix:
+	for symbol in postfix.split():
 		if _is_operator(symbol):
 			right_operand = operand_stack.pop()
 			left_operand = operand_stack.pop()
-			operand_stack.append("(" + left_operand + symbol + right_operand
-			                     + ")")
+			operand_stack.append("(" + left_operand + " "
+			                     + symbol
+			                     + " " + right_operand + ")")
 		else:
 			operand_stack.append(symbol)
 	
-	return ''.join(operand_stack)[1:-1]
+	return ' '.join(operand_stack)[1:-1]
 
 
 def postfix_to_infix(postfix: str) -> str:
@@ -109,16 +108,17 @@ def prefix_to_infix(prefix: str) -> str:
 
 
 def _postfix_to_prefix(postfix: str) -> str:
-	
 	operand_stack: OperandStack = []
 	
-	for symbol in postfix:
+	for symbol in postfix.split():
 		if _is_operand(symbol):
 			operand_stack.append(symbol)
 		else:
 			right_operand = operand_stack.pop()
 			left_operand = operand_stack.pop()
-			operand_stack.append(symbol + left_operand + right_operand)
+			operand_stack.append(symbol
+			                     + " " + left_operand
+			                     + " " + right_operand)
 	
 	return operand_stack.pop()
 
@@ -131,47 +131,45 @@ def postfix_to_prefix(postfix: str) -> str:
 
 def prefix_to_postfix(prefix: str) -> str:
 	"""Return postfix representation of the prefix expression."""
-
+	
 	return _postfix_to_prefix(prefix[::-1])[::-1]
 
 
 def _infix_to_postfix(infix: str, *, pop_function: PopFunction) -> str:
 	"""Return postfix representation of the infix expression."""
-
+	
 	postfix_list: list[str] = []
 	operator_stack: OperatorStack = []
 	
-	for symbol in infix:
+	for symbol in infix.split():
 		if _is_operand(symbol):
 			postfix_list.append(symbol)
-		
 		elif _is_operator(symbol):
 			while operator := pop_function(operator_stack, symbol):
 				postfix_list.append(operator)
 			operator_stack.append(symbol)
-		
 		elif symbol == "(":
 			operator_stack.append(symbol)
-		
 		elif symbol == ")":
-			while len(operator_stack) \
-					and (popped_operator := operator_stack.pop()) != "(":
+			while (len(operator_stack)
+			       and (popped_operator := operator_stack.pop()) != "("):
 				postfix_list.append(popped_operator)
 	
 	while len(operator_stack):
 		postfix_list.append(operator_stack.pop())
 	
-	return ''.join(postfix_list)
+	return ' '.join(postfix_list)
 
 
 def infix_to_prefix(infix: str) -> str:
 	"""Using a few 'tricks' is faster than converting infix to postfix and then
 	postfix to prefix...'"""
-
+	
 	return _infix_to_postfix(_swap_chars(infix, "(", ")")[::-1],
 	                         pop_function=_pop_prefix)[::-1]
 
 
 def infix_to_postfix(infix: str) -> str:
 	"""After introducing bla"""
+	
 	return _infix_to_postfix(infix, pop_function=_pop_postfix)
--- tests/mathstuff/test_in_pre_post.py
+++ tests/mathstuff/test_in_pre_post.py
@@ -1,8 +1,9 @@
 """Tests all available conversions."""
 from in_pre_postfix import infix_to_postfix, postfix_to_infix, \
-	infix_to_prefix, prefix_to_postfix, prefix_to_infix, postfix_to_prefix
+	infix_to_prefix, prefix_to_postfix, prefix_to_infix, postfix_to_prefix, \
+	__exponential_operator
 
-test_in_post_pre = (
+unspaced_tuple = (
 	("A+B-C", "AB+C-", "-+ABC"),
 	("A+B*C", "ABC*+", "+A*BC"),
 	("(A+B)/(C-D)", "AB+CD-/", "/+AB-CD"),
@@ -23,10 +24,25 @@ test_in_post_pre = (
 )
 
 
+def _get_test_data() -> tuple[list[str]]:
+	
+	return tuple(list(' '.join(string).replace("^", __exponential_operator)
+	                  for string in line)
+	             for line in unspaced_tuple)
+	
+
+test_in_post_pre = _get_test_data()
+
+
 def test_infix_to_postfix() -> None:
 	for infix, postfix, _ in test_in_post_pre:
 		assert infix_to_postfix(infix) == postfix
-
+	
+	infix, postfix, _ = test_in_post_pre[-1]
+	# us_infix, us_postfix, _ = unspaced_tuple[-1]
+	print(f"\n{infix = }, {infix_to_postfix(infix) = }")
+	# print(f"{us_infix = }, {us_postfix = }")
+	
 
 def test_infix_to_prefix() -> None:
 	for infix, _, prefix in test_in_post_pre:
@@ -34,6 +50,11 @@ def test_infix_to_prefix() -> None:
 			f"{infix=}, " \
 			f"expected (my tests): {prefix}, " \
 			f"got: {infix_to_prefix(infix)}"
+
+	infix, _, prefix = test_in_post_pre[-1]
+	# us_infix, _, us_prefix = unspaced_tuple[-1]
+	print(f"\n{infix = }, {infix_to_prefix(infix) = }")
+	# print(f"{us_infix = }, {us_prefix = }")
 
 
 def test_any_to_infix() -> None:
@@ -43,12 +64,26 @@ def test_any_to_infix() -> None:
 		assert infix_from_postfix == infix_from_prefix, \
 			f"{infix = }, {infix_from_postfix = }, {infix_from_prefix = }"
 
+	infix, postfix, prefix = test_in_post_pre[-1]
+	print(f"\n{postfix  = }, {postfix_to_infix(postfix) = }")
+	print(f"{prefix = }, {prefix_to_infix(prefix) = }")
+
 
 def test_prefix_to_postfix() -> None:
 	for _, postfix, prefix in test_in_post_pre:
 		assert prefix_to_postfix(prefix) == postfix
 
+	_, postfix, prefix = test_in_post_pre[-1]
+	# _, us_postfix, us_prefix = unspaced_tuple[-1]
+	print(f"\n{prefix = }, {prefix_to_postfix(prefix) = }")
+	# print(f"{us_prefix = }, {us_postfix = }")
+
 
 def test_postfix_to_prefix() -> None:
 	for _, postfix, prefix in test_in_post_pre:
 		assert postfix_to_prefix(postfix) == prefix
+
+	_, postfix, prefix = test_in_post_pre[-1]
+	# _, us_postfix, us_prefix = unspaced_tuple[-1]
+	print(f"\n{postfix = }, {postfix_to_prefix(postfix) = }")
+	# print(f"{us_postfix = }, {us_prefix = }")
